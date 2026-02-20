7fb831c1a45dbba6282d902009e9da301f22ccd4
AviationLiker <162184282+AviationLiker@users.noreply.github.com>
2026-02-20 15:32:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,15 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const path = require('path');
-
 const app = express();
-const server = http.createServer(app);
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, { cors: { origin: "*" } });
 
-// Allows your WordPress site to talk to this server
-const io = new Server(server, {
-    cors: {
-        origin: "*", 
-        methods: ["GET", "POST"]
-    }
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 let players = {};
 
 io.on('connection', (socket) => {
-    players[socket.id] = {
-        x: 100 + Math.random() * 400,
-        y: 100 + Math.random() * 200,
-        z: 0,
-        color: '#3498db',
-        lastMsg: ""
-    };
-
+    players[socket.id] = { x: 200, y: 200, z: 0, color: '#3498db', nick: "Guest", lastMsg: "" };
     socket.emit('init', { id: socket.id, player: players[socket.id] });
-    io.emit('updatePlayers', players);
 
     socket.on('move', (data) => {
         if (players[socket.id]) {
@@ -42,7 +21,8 @@ io.on('connection', (socket) => {
     socket.on('chatMessage', (msg) => {
         if (players[socket.id]) {
             players[socket.id].lastMsg = msg;
-            io.emit('newChatMessage', { id: socket.id.substr(0, 4), text: msg, color: players[socket.id].color });
+            // Use the nick for the chat log
+            io.emit('newChatMessage', { id: players[socket.id].nick, text: msg });
             io.emit('updatePlayers', players);
             setTimeout(() => {
                 if (players[socket.id]) { players[socket.id].lastMsg = ""; io.emit('updatePlayers', players); }
@@ -53,5 +33,4 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => { delete players[socket.id]; io.emit('updatePlayers', players); });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
+server.listen(process.env.PORT || 3000);
