df8c5c650cbb7dde4319b4b1eccc4452f60fc3b6
AviationLiker <162184282+AviationLiker@users.noreply.github.com>
2026-02-21 11:49:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,32 +6,30 @@ const io = require('socket.io')(server, { cors: { origin: "*" } });
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 let players = {};
-let coin = { x: 400, y: 300 }; // The current coin position
+let coin = { x: 400, y: 300 };
 
 function respawnCoin() {
-    coin.x = 50 + Math.random() * 700;
-    coin.y = 50 + Math.random() * 500;
+    coin.x = 100 + Math.random() * (800 - 200);
+    coin.y = 100 + Math.random() * (600 - 200);
     io.emit('coinUpdate', coin);
 }
 
 io.on('connection', (socket) => {
-    players[socket.id] = { x: 200, y: 200, z: 0, color: '#3498db', nick: "Guest", score: 0, lastMsg: "" };
+    players[socket.id] = { x: 300, y: 300, z: 0, color: '#3498db', nick: "Guest", score: 0, lastMsg: "" };
     
-    socket.emit('init', { id: socket.id, player: players[socket.id], coin: coin });
+    socket.emit('init', { id: socket.id, coin: coin });
 
     socket.on('move', (data) => {
         if (players[socket.id]) {
             Object.assign(players[socket.id], data);
             
-            // Check Collision with Coin (Server-side for anti-cheat)
+            // Server-side coin collision
             let dx = players[socket.id].x - coin.x;
             let dy = (players[socket.id].y - players[socket.id].z) - coin.y;
-            let dist = Math.sqrt(dx*dx + dy*dy);
-            
-            if (dist < 30) {
+            if (Math.sqrt(dx*dx + dy*dy) < 30) {
                 players[socket.id].score++;
                 respawnCoin();
-                io.emit('updatePlayers', players); // Update leaderboard
+                io.emit('updatePlayers', players);
             }
             socket.broadcast.emit('updatePlayers', players);
         }
@@ -42,11 +40,19 @@ io.on('connection', (socket) => {
             players[socket.id].lastMsg = msg;
             io.emit('newChatMessage', { id: players[socket.id].nick, text: msg });
             io.emit('updatePlayers', players);
-            setTimeout(() => { if(players[socket.id]){ players[socket.id].lastMsg = ""; io.emit('updatePlayers', players); }}, 5000);
+            setTimeout(() => { 
+                if(players[socket.id]){ 
+                    players[socket.id].lastMsg = ""; 
+                    io.emit('updatePlayers', players); 
+                }
+            }, 5000);
         }
     });
 
-    socket.on('disconnect', () => { delete players[socket.id]; io.emit('updatePlayers', players); });
+    socket.on('disconnect', () => { 
+        delete players[socket.id]; 
+        io.emit('updatePlayers', players); 
+    });
 });
 
 server.listen(process.env.PORT || 3000);
